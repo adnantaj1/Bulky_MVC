4338ec9170e0b9f9f701104987343c8ea605935d
adnantaj1 <adnansulehri1@gmail.com>
2024-07-09 17:58:10 +0200

    CRUD operation for Category is added, Toaster message is added in _Layout partial, where from every page can access it for notifications.

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace BulkyWeb.Data
 {
     public class ApplicationDbContext : DbContext
@@ -6,6 +8,17 @@ namespace BulkyWeb.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
         {
                 
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder =1},
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
         }
     }
 }
