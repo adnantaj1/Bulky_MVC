93a733c0dc82421f1c835130c95f8b511db7bab0
adnantaj1 <adnansulehri1@gmail.com>
2024-08-26 23:42:23 +0200

    logic to add images in product added

--- Bulky.DataAccess/Repository/ProductRepository.cs
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -14,6 +14,25 @@ namespace BulkyBook.DataAccess.Repository
         public void Update(Product obj)
         {
             _db.Products.Update(obj);
+            /*above we are updating the whole object, but we can manualy update product by our own logic,
+            that is why update is in ProductRepo so we implement logic with respect to product*/
+            var objFromDb = _db.Products.FirstOrDefault(u=>u.Id == obj.Id);
+            if (objFromDb != null) 
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Price = obj.Price;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Description = obj.Description;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.Author = obj.Author;
+                if (obj.ImageUrl != null) 
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
         }
     }
 }
