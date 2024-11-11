328c14b9849ebf33af49731acc08049d90638084
adnantaj1 <adnansulehri1@gmail.com>
2024-11-11 14:05:27 +0100

    Identity module added, UI changes and ViewModel for Apllication User created

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -13,7 +13,12 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(options => {
+    options.LoginPath = $"/identity/Account/Login";
+    options.LogoutPath = $"/identity/Account/Logout";
+    options.AccessDeniedPath = $"/identity/Account/AccessDenied";
+});
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 builder.Services.AddRazorPages();
