6d451e978a5e0f93c044109c5ad34df3ee425523
adnantaj1 <adnansulehri1@gmail.com>
2024-11-12 22:51:53 +0100

    OrderDetail and OrderHeader models and their repos added, Order summry Page updated

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -18,6 +18,8 @@ namespace BulkyBook.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ShoppingCart> shoppingCarts { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
