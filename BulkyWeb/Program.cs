56a2f2b9b820b536a298e510058aae511883bcb3
adnantaj1 <adnansulehri1@gmail.com>
2024-11-14 19:30:01 +0100

    Stripe integrated, on payment order and payment status change logic implemented

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -5,45 +5,56 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Stripe;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options=>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
-builder.Services.ConfigureApplicationCookie(options => {
-    options.LoginPath = $"/identity/Account/Login";
-    options.LogoutPath = $"/identity/Account/Logout";
-    options.AccessDeniedPath = $"/identity/Account/AccessDenied";
-});
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IEmailSender, EmailSender>();
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	private static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		// Add services to the container.
+		builder.Services.AddControllersWithViews();
+		builder.Services.AddDbContext<ApplicationDbContext>(options =>
+			options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+		builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
+
+		builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+		builder.Services.ConfigureApplicationCookie(options =>
+		{
+			options.LoginPath = $"/identity/Account/Login";
+			options.LogoutPath = $"/identity/Account/Logout";
+			options.AccessDeniedPath = $"/identity/Account/AccessDenied";
+		});
+		builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+		builder.Services.AddScoped<IEmailSender, EmailSender>();
+		builder.Services.AddRazorPages();
+
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+		if (!app.Environment.IsDevelopment())
+		{
+			app.UseExceptionHandler("/Home/Error");
+			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+			app.UseHsts();
+		}
+
+		app.UseHttpsRedirection();
+		app.UseStaticFiles();
+		StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+
+		app.UseRouting();
+
+		app.UseAuthentication();
+		app.UseAuthorization();
+		app.MapRazorPages();
+
+		app.MapControllerRoute(
+			name: "default",
+			pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
+		app.Run();
+	}
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapRazorPages();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
