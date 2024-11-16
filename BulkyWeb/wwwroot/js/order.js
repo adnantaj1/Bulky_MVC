499e9a919cf8d7032684b7fbc46fdb63e905fab7
adnantaj1 <adnansulehri1@gmail.com>
2024-11-16 17:54:58 +0100

    order managment added

--- BulkyWeb/wwwroot/js/order.js
+++ BulkyWeb/wwwroot/js/order.js
@@ -1,15 +1,30 @@
 ﻿var dataTable;
 $(document).ready(function () {
-    loadDataTable();
+    var statuses = ["inprocess", "completed", "pending", "approved"];
+    var url = window.location.search;
+    var statusFound = false;
+
+    for (var i = 0; i < statuses.length; i++) {
+        if (url.includes(statuses[i])) {
+            loadDataTable(statuses[i]);
+            statusFound = true;
+            break;
+        }
+    }
+
+    if (!statusFound) {
+        loadDataTable("all");
+    }
 });
 
-function loadDataTable() {
+
+function loadDataTable(status) {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url: '/admin/order/getAll' },
+        "ajax": { url: '/admin/order/getAll?status=' + status },
         "columns" : [
             { data: 'id', "width": "5%" },
-            { data: 'name', "width": "15%" },
-            { data: 'phoneNumber', "width": "15%" },
+            { data: 'name', "width": "25%" },
+            { data: 'phoneNumber', "width": "20%" },
             { data: 'applicationUser.email', "width": "20%" },
             { data: 'orderStatus', "width": "10%" },
             { data: 'orderTotal', "width": "10%" },
@@ -20,7 +35,7 @@ function loadDataTable() {
                     <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>
                     </div>`
                 },
-                "width": "25%"
+                "width": "10%"
             },
         ]
     });
