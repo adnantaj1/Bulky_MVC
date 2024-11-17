3993b59bc0aabe8ddcd4798608a8fc469b640abb
adnantaj1 <adnansulehri1@gmail.com>
2024-11-17 20:40:12 +0100

    DbInitizer added, so for first time admin automatically created and all the necessary migration should be applied

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Stripe;
+using BulkyBook.DataAccess.DbInitializer;
 
 internal class Program
 {
@@ -44,6 +45,7 @@ internal class Program
 			options.Cookie.IsEssential = true;
 		});
 
+		builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 		builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 		builder.Services.AddScoped<IEmailSender, EmailSender>();
 		builder.Services.AddRazorPages();
@@ -67,6 +69,7 @@ internal class Program
 		app.UseAuthentication();
 		app.UseAuthorization();
 		app.UseSession();
+		SeedDatabase();
 		app.MapRazorPages();
 
 		app.MapControllerRoute(
@@ -74,5 +77,14 @@ internal class Program
 			pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 		app.Run();
+
+		void SeedDatabase()
+		{
+			using (var scope = app.Services.CreateScope())
+			{
+				var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+				dbInitializer.Initialize();
+			}
+		}
 	}
 }
